96ae22f056c49ca74b249e050739dec568a5ceb2
sunidhisuresh37-oss <sunidhisuresh37@gmail.com>
2026-03-06 16:23:50 +0530

    Update lung_cancer_model.py

--- lung_cancer_model.py
+++ lung_cancer_model.py
@@ -23,7 +23,7 @@ y = data["LUNG_CANCER"]
 
 # Train test split
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
+    X, y, test_size=0.2, random_state=32
 )
 
 # Model
